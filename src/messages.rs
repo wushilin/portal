3a89de7c4f42333954b90a7ae1d1566ca0c43c3e
Wu Shilin <wushilin.sg@gmail.com>
2025-12-24 17:35:59 +0800

    Added coker

--- src/messages.rs
+++ src/messages.rs
@@ -1,3 +1,20 @@
-pub struct OMG {
+pub struct OMG {}
 
+impl OMG {
+    pub fn new() -> Self {
+        Self {}
+    }
+    pub fn say_omg(&self) {
+        println!("OMG!");
+    }
+
+    pub fn say_omg_with_exclamation(&self) {
+        println!("OMG!");
+    }
+    pub fn say_omg_with_exclamation_mark(&self) {
+        println!("OMG!");
+    }
+    pub fn say_omg_with_exclamation_mark_and_exclamation(&self) {
+        println!("OMG!");
+    }
 }
